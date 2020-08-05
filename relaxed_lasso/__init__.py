fe53456b906943b391c35aadf38746524a63d05e
Flop-py <65293477+Flop-py@users.noreply.github.com>
2020-08-05 12:16:06 +0200

    Include relasso_cd in package

--- relaxed_lasso/__init__.py
+++ relaxed_lasso/__init__.py
@@ -1,4 +1,8 @@
 from .least_angle import RelaxedLassoLars, RelaxedLassoLarsCV
-from. least_angle import relasso_lars_path
+from .least_angle import relasso_lars_path
 
-__all__ = ['RelaxedLassoLars', 'RelaxedLassoLarsCV', 'relasso_lars_path']
+from .coordinate_descent import RelaxedLasso, RelaxedLassoCV
+from .coordinate_descent import relasso_path
+
+__all__ = ['RelaxedLassoLars', 'RelaxedLassoLarsCV', 'relasso_lars_path',
+           'RelaxedLasso', 'RelaxedLassoCV', 'relasso_path']
