5aed1f2a500aaf1b63b98f620c87a9241f904061
Gregory Vial <greg_vial@hotmail.com>
2021-10-07 10:39:26 +0200

    Fix bug in CV wrong choice of alpha theta values

--- relaxed_lasso/least_angle.py
+++ relaxed_lasso/least_angle.py
@@ -833,8 +833,7 @@ class RelaxedLassoLarsCV(RelaxedLassoLars):
         i_best_alpha_reg = i_best_alpha_reg_[0]
         i_best_alpha_var = i_best_alpha_var_[0]
 
-        best_alpha_reg = all_alphas[i_best_alpha_reg]
-        # Increment alpha_var by 1 as we skip 0 (first value in all_alphas)
+        best_alpha_reg = all_alphas[i_best_alpha_reg+1]
         best_alpha_var = all_alphas[i_best_alpha_var]
         best_theta = best_alpha_reg / best_alpha_var
 
